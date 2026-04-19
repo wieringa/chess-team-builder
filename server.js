5ad7f563a6a0c51c2cfe555a6ed097f2ed2b445a
Helmer Wieringa <helmer.wieringa@gmail.com>
2026-04-19 16:39:26 +0200

    add dotenv

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 require("dotenv").config();
 
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
+
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
